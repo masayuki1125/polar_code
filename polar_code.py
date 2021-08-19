512305a68e150f45a6cdcda662728dfd2dcfd669
masayuki1125 <masayuki19981125@gmail.com>
2021-08-19 12:54:46 +0900

    change

--- polar_code.py
+++ polar_code.py
@@ -1,50 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# In[32]:
 
 
 #必要なライブラリ、定数
+import sys
+sys.path.append("../channel")
+from AWGN import _AWGN
+
 import numpy as np
 import random
 import time
 import math
-import copy
 from decimal import *
 random.seed(time.time())
-#below is the info of basemat
-N=1024 #log_2(N)==int <-- requirement
-K=512
-design_SNR=4
 
-itr_num=np.log2(N)
-itr_num=itr_num.astype(int)
+ch=_AWGN()
 
 
-# In[16]:
+# In[44]:
 
 
-G2=np.array([[1,0],[1,1]],dtype=np.int)
+class coding():
+    def __init__(self,N):
+        super().__init__()
 
-def tensordot(A):
-  tmp0=np.zeros((A.shape[0],A.shape[1]),dtype=np.int)
-  tmp1=np.append(A,tmp0,axis=1)
-  #print(tmp1)
-  tmp2=np.append(A,A,axis=1)
-  #print(tmp2)
-  tmp3=np.append(tmp1,tmp2,axis=0)
-  #print(tmp3)
-  return tmp3
+        self.N=N
+        self.R=0.5
+        self.K=math.floor(self.R*self.N)
+        self.design_SNR=4
 
-Gres=G2
-for i in range(itr_num-1):
-  #print(i)
-  Gres=tensordot(Gres)
-print(Gres)
-print(Gres.shape)
+        #prepere constants
+        tmp2=np.log2(self.N)
+        tmp2=tmp2.astype(int)
+        self.itr_num=tmp2
+        self.frozen_bits,self.info_bits=            Bhattacharyya_bounds(self.N,self.K,self.design_SNR)
+
+        self.Gres=make_H(self.itr_num)
+
+        self.filename="polar_code_{}_{}".format(self.N,self.K)
+        
 
 
-# In[17]:
+# In[34]:
+
+
+#cd=coding(1024)
+#a=512.0
+#a=a.astype(int)
+#print(a)
+
+
+# In[35]:
 
 
 #frozen_bitの選択
@@ -70,142 +78,268 @@ def Bhattacharyya_bounds(N,K,EdB):
       z[tmp+t]=Decimal(str(T))**Decimal('2')
   #print(z)
   #np.savetxt("z",z)
-  res=indices_of_elements(z,N-K)
-
-  return res
+  tmp=indices_of_elements(z,N)
+  frozen_bits=tmp[:N-K]
+  info_bits=tmp[N-K:]
+  return np.sort(frozen_bits),np.sort(info_bits)
 
 def indices_of_elements(v,l):
   tmp=np.argsort(v)[::-1]
-  print(tmp)
+  #print(tmp)
   res=tmp[0:l]
   return res
 
-#frozen bit を定める
 
-filename="frozen_bits_{}dB.txt".format(design_SNR)
-frozen_bit=np.loadtxt(filename,dtype=int)
-
-#frozen_bit=Bhattacharyya_bounds(N,K,design_SNR)
+# In[36]:
 
 
-
-frozen_bit=frozen_bit[:N-K]
-print(frozen_bit.shape)
-if frozen_bit.shape[0]!=N-K:
-  print("frozen_bit err")
-  exit()
+def generate_information(K):
+      #generate information
+  information=np.random.randint(0,2,K)
+  return information
 
 
-# In[18]:
+# In[37]:
 
 
-def generate_codeword(N,Gres):
-  S=np.zeros(N)
-  for i in range(N):
-    S[i]=random.randrange(0,2)
-    np.put(S,frozen_bit,0)
-  U=(S@Gres)%2
-  #print(U.shape)
-  return S,U
-
-S,U=generate_codeword(N,Gres)
-print(S,U)
+def generate_U(N,information,info_bits):
+    u_message=np.zeros(N)
+    u_message[info_bits]=information
+    return u_message
 
 
-# In[19]:
+# In[38]:
 
 
-def AWGN_channel(EbNodB,codeword):
-  #modulation-channel
-  # 送信側アンテナ数
-  M =1
-  # 受信側アンテナ数
-  N = 1
-  # 送信ビット列
-  TX_bit =copy.deepcopy(codeword)
-  # 送信ビット数
-  bit_num =codeword.shape[1]
+def encode(u_message):
+    """
+    Implements the polar transform on the given message in a recursive way (defined in Arikan's paper).
+    :param u_message: An integer array of N bits which are to be transformed;
+    :return: codedword -- result of the polar transform.
+    """
+    u_message = np.array(u_message)
 
-  # Additive Gaussian White Noiseの生成する際のパラメータ設定
-  EbNo = 10 ** (EbNodB / 10)
-  No=1/EbNo #Eb=1(fixed)
+    if len(u_message) == 1:
+        codeword = u_message
+    else:
+        u1u2 = np.logical_xor(u_message[::2] , u_message[1::2])
+        u2 = u_message[1::2]
 
-  # 0 -> 1, 1 -> -1としてBPSK変調
-  TX_BPSK = TX_bit
-  TX_BPSK[TX_bit==1]=-1
-  TX_BPSK[TX_bit==0]=1
-
-  # AWGN雑音の生成
-  noise = np.random.normal(0, np.sqrt(No / 2), (M, bit_num))           #+ 1j * np.random.normal(0, np.sqrt(No / 2), (M, bit_num))
-
-  # AWGN通信路 = 送信シンボル間干渉が生じないような通信路で送信
-  RX_BPSK = TX_BPSK + noise
-
-  # 以下のprint関数の出力を表示すると、Noとほぼ一致するはず
-  #print(np.dot(noise[0, :], np.conj(noise[0, :]))/bit_num)
-  
-  return RX_BPSK
+        codeword = np.concatenate([encode(u1u2), encode(u2)])
+    return codeword
 
 
-# In[20]:
+# In[39]:
 
 
-def chk(llr_1,llr_2):
-  CHECK_NODE_TANH_THRES=30
-  res=np.zeros(len(llr_1))
-  for i in range(len(res)):
+def tensordot(A):
 
-    if abs(llr_1[i]) > CHECK_NODE_TANH_THRES and abs(llr_2[i]) > CHECK_NODE_TANH_THRES:
-      if llr_1[i] * llr_2[i] > 0:
-        # If both LLRs are of one sign, we return the minimum of their absolute values.
-        res[i]=min(abs(llr_1[i]), abs(llr_2[i]))
+  tmp0=np.zeros((A.shape[0],A.shape[1]),dtype=np.int)
+  tmp1=np.append(A,tmp0,axis=1)
+  #print(tmp1)
+  tmp2=np.append(A,A,axis=1)
+  #print(tmp2)
+  tmp3=np.append(tmp1,tmp2,axis=0)
+  #print(tmp3)
+  return tmp3
+
+def make_H(itr_num):
+  G2=np.array([[1,0],[1,1]],dtype=np.int)
+  Gres=G2
+  for i in range(itr_num-1):
+    #print(i)
+    Gres=tensordot(Gres)
+  return Gres
+
+'''
+N=16
+K=8
+itr_num=np.log2(N)
+itr_num=itr_num.astype(int)
+
+Gres=make_H(itr_num)
+#print(Gres)
+
+cd=coding()
+info=generate_information(K)
+print(info)
+inbits=np.sort(cd.info_bits)
+print(inbits)
+
+u_message=generate_U(N,info,inbits)
+print(u_message)
+S1=(u_message@Gres)%2
+print(S1)
+S2=encode(u_message)
+print(S2)
+
+Lc=-1*ch.generate_LLR(S2,100)
+print(Lc)
+
+dc=decoding()
+dc.SC_decoding(Lc)
+res=dc.EST_information
+print(res)
+res=-1*np.sign(res)
+codeword=(res+1)/2
+print(codeword)
+
+#codeword=codeword[inbits]
+#print(codeword)
+codeword=codeword[inbits]
+print(codeword)
+'''
+
+
+# In[40]:
+
+
+#0,1が逆になって設計されているので、ちゃんと治す必要あり
+
+class decoding(coding):
+  n=0
+  EST_information=np.array([])
+
+  def __init__(self,N):
+    super().__init__(N)
+
+  @staticmethod
+  def chk(llr_1,llr_2):
+    CHECK_NODE_TANH_THRES=30
+    res=np.zeros(len(llr_1))
+    for i in range(len(res)):
+
+      if abs(llr_1[i]) > CHECK_NODE_TANH_THRES and abs(llr_2[i]) > CHECK_NODE_TANH_THRES:
+        if llr_1[i] * llr_2[i] > 0:
+          # If both LLRs are of one sign, we return the minimum of their absolute values.
+          res[i]=min(abs(llr_1[i]), abs(llr_2[i]))
+        else:
+          # Otherwise, we return an opposite to the minimum of their absolute values.
+          res[i]=-1 * min(abs(llr_1[i]), abs(llr_2[i]))
       else:
-        # Otherwise, we return an opposite to the minimum of their absolute values.
-        res[i]=-1 * min(abs(llr_1[i]), abs(llr_2[i]))
-    else:
-      res[i]= 2 * np.arctanh(np.tanh(llr_1[i] / 2, ) * np.tanh(llr_2[i] / 2))
-  return res
+        res[i]= 2 * np.arctanh(np.tanh(llr_1[i] / 2, ) * np.tanh(llr_2[i] / 2))
+    return res
 
+  def SC_decoding(self,a):
+    #interior node operation
+    if a.shape[0]==1:
+      #frozen_bit or not
+      if np.any(self.frozen_bits==decoding.n):
+        tmp0=np.zeros(1)
+      elif a>=0:
+        tmp0=np.zeros(1)
+      elif a<0:
+        tmp0=np.ones(1)
+      else:
+        print("err!")
+        exit()
+      
+      if np.any(self.info_bits==decoding.n):
+        decoding.EST_information=np.append(decoding.EST_information,a)
+      #print(decoding.n)
+      #print(t)
+      decoding.n+=1
+      #if t>=N:
+        #exit()
+      return tmp0
 
-def SC_decoding(a):
-  global t
-  #interior node operation
-  if a.shape[0]==1:
-    #frozen_bit or not
-    if np.any(frozen_bit==t):
-      tmp0=np.zeros(1)
-    elif a>=0:
-      tmp0=np.zeros(1)
-    elif a<0:
-      tmp0=np.ones(1)
-    else:
-      print("err!")
-      exit()
-    EST_codeword[t]=tmp0
-    #print(t)
-    t+=1
-    #if t>=N:
-      #exit()
-    return tmp0
+    #step1 left input a output u1_hat
 
-  #step1 left input a output u1_hat
-  tmp1=np.split(a,2)
-  f_half_a=chk(tmp1[0],tmp1[1])
-  u1=SC_decoding(f_half_a)
+    tmp1=np.split(a,2)
+    f_half_a=self.chk(tmp1[0],tmp1[1])
+    u1=self.SC_decoding(f_half_a)
 
-  #step2 right input a,u1_hat output u2_hat 
-  tmp2=np.split(a,2)
-  g_half_a=tmp2[1]+(1-2*u1)*tmp2[0] 
-  u2=SC_decoding(g_half_a)
+    #step2 right input a,u1_hat output u2_hat 
+    tmp2=np.split(a,2)
+    g_half_a=tmp2[1]+(1-2*u1)*tmp2[0] 
+    u2=self.SC_decoding(g_half_a)
   
-  #step3 up input u1,u2 output a_hat
-  res=np.concatenate([(u1+u2)%2,u2])
-  return res
+    #step3 up input u1,u2 output a_hat
+    res=np.concatenate([(u1+u2)%2,u2])
+    return res
 
 
-# In[21]:
+# In[41]:
 
 
+'''
+import numpy as np
+
+class parent():
+    def __init__(self):
+        self.a=10
+
+class child(parent):
+    @classmethod
+    def set_v(cls):
+        child.i=0
+        b=np.zeros(cls.a)
+
+    def func(self):
+        if child.ser_v.i<5:
+            child.ser_v.i+=1
+            print(child.ser_v.i)
+            print("p.a=",self.a)
+            child.b[child.ser_v.i]=child.ser_v.i
+            print(child.ser_v.b)
+            c=child()
+            c.func()
+
+
+c=child()
+c.func()
+print("i",child.i)
+print("b",child.b)
+'''
+
+
+# In[42]:
+
+
+class polar_code(decoding):
+    def __init__(self,N):
+        super().__init__(N)
+
+    def encode(self):
+        information=generate_information(self.K)
+        #information=np.array([1,0,0,1])
+        u_message=generate_U(self.N,information,self.info_bits)
+        codeword=(u_message@self.Gres)%2
+        #codeword=encode(u_message)
+        return information,codeword
+
+    def decode(self,Lc):
+        self.SC_decoding(Lc)
+        res=self.EST_information #デコーダが＋、ー逆になってしまうので-１をかける
+        res=-1*np.sign(res)
+        res=(res+1)/2
+        return res
+
+    def polar_code(self,EbNodB):
+        information,codeword=self.encode()
+        Lc=-1*ch.generate_LLR(codeword,EbNodB)#デコーダが＋、ー逆になってしまうので-１をかける
+        EST_information=self.decode(Lc)
+        return information,EST_information
+
+
+# In[43]:
+
+
+if __name__=="__main__":
+    pc=polar_code(512)
+    print(len(pc.info_bits))
+
+    information,EST_information=pc.polar_code(100)
+    print(len(information))
+    print(len(EST_information))
+    print(np.sum(information!=EST_information))
+    
+
+
+# In[ ]:
+
+
+'''
 EbNodB_range=np.arange(0,5.5,0.5)
 BLER=np.zeros(len(EbNodB_range))
 BER=np.zeros(len(EbNodB_range))
@@ -256,7 +390,8 @@ for i,EbNodB in enumerate(EbNodB_range):
     count_berr+=np.sum(sourcecode!=EST_codeword)
     count_ball+=K
 
-    print("\r","count_all=",count_all,",count_err=",count_err,"count_ball="          ,count_ball,"count_berr=",count_berr,end="")
+    print("\r","count_all=",count_all,",count_err=",count_err,"count_ball="\
+          ,count_ball,"count_berr=",count_berr,end="")
 
   print("\n",EbNodB,"BLER=",count_err/count_all,"BER=",count_berr/count_ball)
 
@@ -269,7 +404,7 @@ for i,EbNodB in enumerate(EbNodB_range):
 
 #output "BLER"
 
-filename="polarLLR_ex_{}_{},SN_des={}".format(N,K,design_SNR)
+filename="polarLLR_{}_{},SN_des={}".format(N,K,design_SNR)
 
 with open(filename,'w') as f:
 
@@ -278,4 +413,5 @@ with open(filename,'w') as f:
     print("#EsNodB,BLER,BER",file=f)      #この説明はプログラムによって変えましょう！！！！！！！
     for i in range(len(EbNodB_range)):
         print(str(EbNodB_range[i]),str(BLER[i]),str(BER[i]),file=f)
+'''
 
